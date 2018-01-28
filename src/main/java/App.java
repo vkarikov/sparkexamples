b81403353155036379a6d6c52064bd12d0e6c189
Valeriy Karikov <Valeriy.Karikov@gmail.com>
2018-01-29 00:00:53 +0300

    logging

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,11 +7,13 @@ import org.apache.spark.sql.SparkSession;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
 
         SparkConf conf = new SparkConf()
+                .set("driverClassName", "net.sf.log4jdbc.DriverSpy")
                 .setAppName("Spark user-activity").setMaster("local[2]") //local - означает запуск в локальном режиме.
                 .set("spark.driver.host", "localhost");//это тоже необходимо для локального режима
+
 
         SparkSession spark = SparkSession
                 .builder()
@@ -19,11 +21,19 @@ public class App {
                 .config(conf)
                 .getOrCreate();
 
+        Class.forName("net.sf.log4jdbc.DriverSpy");
+
         Properties connectionProperties = new Properties();
         connectionProperties.put("user", "vkarikov");
         connectionProperties.put("password", "vkarikov");
+        connectionProperties.put("partitionColumn", "id");
+        connectionProperties.put("lowerBound", "1");
+        connectionProperties.put("upperBound", "1000");
+        connectionProperties.put("numPartitions", "10");
+
+
         Dataset<Row> jdbcDF2 = spark.read()
-                .jdbc("jdbc:oracle:thin:@localhost:1521:xe", "VKARIKOV.ULOG", connectionProperties);
+                .jdbc("jdbc:log4jdbc:oracle:thin:@localhost:1521:xe", "VKARIKOV.ULOG2", connectionProperties);
 
         jdbcDF2.show();
     }
